b431449e514598dd3ccd224162908d847f900237
wu_mac <wuq_xd@163.com>
2020-08-07 22:05:15 +0800

    初次提交

---
+++ src/pages/home/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div><i-button type="primary" bind:click="handleClick">这是一个按钮</i-button></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      motto: 'Hello miniprograme',
+      userInfo: {
+        nickName: 'mpvue',
+        avatarUrl: 'http://mpvue.com/assets/logo.png'
+      }
+    }
+  },
+
+  components: {},
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    },
+    clickHandle (ev) {
+      console.log('clickHandle:', ev)
+      // throw {message: 'custom test'}
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+</style>
