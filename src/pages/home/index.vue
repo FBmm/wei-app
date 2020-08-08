b1b41dd64dc209de50d925909f4391bf67705bce
wu_mac <wuq_xd@163.com>
2020-08-08 23:30:16 +0800

    调整首页

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,41 +1,28 @@
 <template>
-  <div><i-button type="primary" bind:click="handleClick">这是一个按钮</i-button></div>
+  <div class="home__container">
+    <!-- <div><i-button type="primary" bind:click="handleClick">这是一个按钮</i-button></div> -->
+    <div></div>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
-    }
+    return {}
   },
 
   components: {},
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
-    },
     clickHandle (ev) {
       console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
     }
   },
-
-  created () {
-    // let app = getApp()
-  }
 }
 </script>
 
 <style scoped>
+.home__container {
+
+}
 </style>
